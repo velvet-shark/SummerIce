1340ca6382ad51cd2c4f119f04eb056b9f3f610d
Radek Sienkiewicz <mail@velvetshark.com>
2024-03-07 01:08:21 +0100

    Default summary length, update styling

--- background.js
+++ background.js
@@ -9,3 +9,50 @@ chrome.runtime.onInstalled.addListener(function (details) {
     chrome.tabs.create({ url: "setup.html" });
   }
 });
+
+// Listen for commands
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "_execute_action") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: activeTab.id },
+          files: ["content.js"]
+        },
+        () => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          }
+        }
+      );
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "summarizePage") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const url = tabs[0].url;
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          function: function () {
+            return document.body.textContent;
+          }
+        },
+        (result) => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          } else {
+            // Call the summarizeText function with the URL
+            summarizeText(url).then((summary) => {
+              // Send the summary to the popup
+              chrome.runtime.sendMessage({ summary: summary });
+            });
+          }
+        }
+      );
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,62 +1,39 @@
-// let screenWidth = window.screen.width;
-// document.body.style.minWidth = screenWidth * 0.3 + "px";
-// document.body.style.minWidth = "600px";
-// document.body.style.maxWidth = "800px";
-// document.body.style.maxHeight = "1400px";
-
 window.onload = function () {
   var summaryArea = document.getElementById("summary-area");
-  if (summaryArea.innerHTML.trim() === "") {
+  if (summaryArea && summaryArea.innerHTML.trim() === "") {
     summaryArea.style.display = "none";
   }
 };
 
-// Function to fetch the article text
-function fetchArticleText() {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          function: function () {
-            return document.body.innerText;
-          }
-        },
-        (result) => {
-          if (chrome.runtime.lastError) {
-            reject(chrome.runtime.lastError);
-            console.log("Rejected");
-          } else {
-            console.log(result[0].result);
-            resolve(result[0].result);
-          }
-        }
-      );
-    });
-  });
-}
-
-async function summarizeText(text, length) {
-  // Check if both text and length are provided
-  if (!text || !length) {
-    console.error("Invalid parameters provided to summarizeText");
+async function summarizeText(url) {
+  // Check if URL is passed as an argument
+  if (!url) {
+    console.error("URL of the article not provided");
     return;
   }
 
   try {
     // Get the API key from local storage
-    const apiKey = await chrome.storage.local.get("apiKey");
+    // const apiKey = await chrome.storage.local.get("apiKey");
+    const apiKey = await new Promise((resolve) => {
+      chrome.storage.local.get("apiKey", (result) => {
+        resolve(result.apiKey);
+      });
+    });
 
-    let url = "https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions";
+    if (!apiKey) {
+      throw new Error("API key not found in chrome.storage.local");
+    }
+
     let options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey.apiKey}`
+        Authorization: `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        prompt: `Please provide a concise and comprehensive summary of the given text. The summary should capture the main points and key details of the text while conveying the author's intended meaning accurately. Please ensure that the summary is well-organized and easy to read, with clear headings and subheadings to guide the reader through each section. The length of the summary should be so that it can be read in ${length}. Do not exceed this reading time, under any circumstances. Check the length of the summary before returning it. The summary should be appropriate to capture the main points and key details of the text, without including unnecessary information or becoming overly long. \n\nText to summarize:\n\n${text}`,
-        max_tokens: 500
+        prompt: `Please provide a concise and comprehensive summary of the article at URL: ${url}. The summary should capture the main points and key details of the text while conveying the author's intended meaning accurately. Please ensure that the summary is well-organized and easy to read, with clear headings and subheadings to guide the reader through each section. The length of the summary should be so that it can be read in under 1 minute. Try to not exceed this reading time. The summary should be appropriate to capture the main points and key details of the text, without including unnecessary information or becoming overly long.`,
+        max_tokens: 3800
       })
     };
     // Send the data to the background page
@@ -68,11 +45,11 @@ async function summarizeText(text, length) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey.apiKey}`
+        Authorization: `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        prompt: `Please provide a concise and comprehensive summary of the given text. The summary should capture the main points and key details of the text while conveying the author's intended meaning accurately. Please ensure that the summary is well-organized and easy to read, with clear headings and subheadings to guide the reader through each section. The length of the summary should be so that it can be read in ${length}. Try to not exceed this reading time. The summary should be appropriate to capture the main points and key details of the text, without including unnecessary information or becoming overly long. \n\nText to summarize:\n\n${text}`,
-        max_tokens: 500
+        prompt: `Please provide a concise and comprehensive summary of the article at URL: ${url}. The summary should capture the main points and key details of the text while conveying the author's intended meaning accurately. Please ensure that the summary is well-organized and easy to read, with clear headings and subheadings to guide the reader through each section. The length of the summary should be so that it can be read in under 1 minute. Try to not exceed this reading time. The summary should be appropriate to capture the main points and key details of the text, without including unnecessary information or becoming overly long.`,
+        max_tokens: 3800
       })
     });
 
@@ -93,32 +70,42 @@ async function summarizeText(text, length) {
 
 // Function to display the summary
 function displaySummary(summary) {
-  var summaryArea = document.getElementById("summary-area");
-  summaryArea.style.display = "block"; // Show the div
-  summaryArea.innerText = summary;
+  const summaryArea = document.getElementById("summary-area");
+  const spinner = document.getElementById("spinner");
+
+  if (summary) {
+    summaryArea.style.display = "block";
+    summaryArea.innerText = summary;
+    spinner.style.display = "none";
+  } else {
+    summaryArea.style.display = "none";
+    spinner.style.display = "block";
+  }
 }
 
 // Function to set up the event listeners
 function setupEventListeners() {
   document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("15s").addEventListener("click", () => {
-      fetchArticleText()
-        .then((text) => {
-          return summarizeText(text, "15 seconds");
-        })
-        .then((summary) => {
-          displaySummary(summary);
-        });
-    });
-
-    document.getElementById("1m").addEventListener("click", () => {
-      fetchArticleText()
-        .then((text) => {
-          return summarizeText(text, "1 minute");
-        })
-        .then((summary) => {
-          displaySummary(summary);
-        });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const url = tabs[0].url;
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          function: function () {
+            return document.body.textContent;
+          }
+        },
+        (result) => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          } else {
+            displaySummary(null); // Show the spinner
+            summarizeText(url).then((summary) => {
+              displaySummary(summary); // Hide the spinner and display the summary
+            });
+          }
+        }
+      );
     });
   });
 }
